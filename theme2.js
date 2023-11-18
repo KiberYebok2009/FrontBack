9cb99b35f7f49045dde3ed063a422d4aa7361aa8
KiberYebok2009 <126006305+KiberYebok2009@users.noreply.github.com>
2023-11-18 23:36:25 +0300

    Add files via upload

--- theme2.js
+++ theme2.js
@@ -6,14 +6,39 @@ const image = document.querySelector('.logo');
 let line = document.querySelector('.header-line');
 let body = document.querySelector('body');
 let chipizdrik = document.querySelector('.chipizdrik');
+let comm = document.querySelectorAll('.comment');
+let avotar = document.querySelectorAll('.avotar');
+let otziv = document.querySelectorAll('.comm');
+let commline = document.querySelectorAll('.comm-line');
+let nik = document.querySelectorAll('.nik');
+
+// let hui = document.getElementsByClassName("comm")['0'].textContent;
+// console.log(hui)
+// console.log(otziv[0].textContent)
+// console.log(nik[0].textContent)             Работает
+// nik[0].textContent = otziv[0].textContent; 
+
+// console.log(avotar[0]);
+// let her = avotar[0].getAttribute("src")     Работает
+// chipizdrik.setAttribute('src', her);
+
 button.onclick = function(){
     header.classList.toggle('first1')
     main.classList.toggle('first2')
     footer.classList.toggle('first3')
+    for (var i = 0; i < comm.length; ++i) {
+        comm[i].classList.toggle('first4');
+    };
+
     header.classList.toggle('second1')
     main.classList.toggle('second2')
     footer.classList.toggle('second3')
+    for (var i = 0; i < comm.length; ++i) {
+        comm[i].classList.toggle('second4');
+    };
+
     image.classList.toggle('logo')
+
     if (image.classList.contains("logo")){
         image.setAttribute('src', '../images/amnyam.webp');
         image.style.borderRadius = "0%";
@@ -21,6 +46,12 @@ button.onclick = function(){
         body.style.backgroundColor = "white";
         chipizdrik.setAttribute('src', '../images/hzshto.jpg');
         chipizdrik.style.border = "solid black 5px"
+        for (var i = 0; i < comm.length; ++i) {
+            avotar[i].setAttribute('src', '../images/amnyam.webp');
+            otziv[i].textContent = "Всё честно";
+            commline[i].style.border = "solid black 5px"
+            otziv[i].style.border = "solid black 5px"
+        };
     }
     else{
         confirm("Ебать хохлов?")
@@ -32,5 +63,11 @@ button.onclick = function(){
         body.style.backgroundColor = "black";
         chipizdrik.setAttribute('src', '../images/boom.gif');
         chipizdrik.style.border = "solid white 5px"
+        for (var i = 0; i < comm.length; ++i) {
+            avotar[i].setAttribute('src', '../images/om nom.jpg');
+            otziv[i].textContent = "НАЕБАЛОВО ПОЛНОЕ";
+            commline[i].style.border = "solid white 5px"
+            otziv[i].style.border = "solid white 5px"
+        };
     }
 }
